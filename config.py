3e32277cbdab8ea9de7757d84ea3c4b06f33e5cf
lzmisscc <1028741371@qq.com>
2020-10-31 17:32:54 +0800

    创建配置文件

---
+++ config.py
@@ -0,0 +1,20 @@
+# construct the argument parser and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", type=str,
+#                 help="path to input image")
+# ap.add_argument("-east", "--east", type=str,
+#                 help="path to input EAST text detector")
+# ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
+#                 help="minimum probability required to inspect a region")
+# ap.add_argument("-w", "--width", type=int, default=320,
+#                 help="resized image width (should be multiple of 32)")
+# ap.add_argument("-e", "--height", type=int, default=320,
+#                 help="resized image height (should be multiple of 32)")
+# args = vars(ap.parse_args())
+args = {
+    "image": "images/car_wash.png",
+    "east": "",
+    "min-confidence": 0.5,
+    "width": 320,
+    "height": 320
+}
--- opencv_text_detection_image.py
+++ opencv_text_detection_image.py
@@ -5,23 +5,9 @@
 # import the necessary packages
 from imutils.object_detection import non_max_suppression
 import numpy as np
-import argparse
 import time
 import cv2
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str,
-                help="path to input image")
-ap.add_argument("-east", "--east", type=str,
-                help="path to input EAST text detector")
-ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
-                help="minimum probability required to inspect a region")
-ap.add_argument("-w", "--width", type=int, default=320,
-                help="resized image width (should be multiple of 32)")
-ap.add_argument("-e", "--height", type=int, default=320,
-                help="resized image height (should be multiple of 32)")
-args = vars(ap.parse_args())
 
 # load the input image and grab the image dimensions
 image = cv2.imread(args["image"])
